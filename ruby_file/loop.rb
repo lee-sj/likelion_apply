a61dbd7eb1a874b18ee86e9d01d9c799afe8d7d4
lee-sj <201302386@hufs.ac.kr>
2019-03-07 17:31:51 +0900

    Update loop.rb

--- ruby_file/loop.rb
+++ ruby_file/loop.rb
@@ -13,16 +13,17 @@ univ = mechanize.get('https://apply.likelion.org/apply/univ/NUMBER')
 
 name = []
 list = []
-
+major = []
 ## 정보들 모으는 과정 
 # NUMBER 에 본인 학교에서 지원서를 제출한 인원의 숫자를 적습니다. 
 (1..NUMBER).each do |i|
 name[i] = univ.search("#likelion_num .applicant_page a:nth-child(#{i}) .user_name").text
+major[i] = univ.search("#likelion_num .applicant_page a:nth-child(#{i}) .user_profile").text.split(" ")[-1]
 list[i] = univ.search("#likelion_num .applicant_page a:nth-child(#{i})")
 link = univ.link_with(text: "#{list[i].text}")
 user = link.click
 number = user.uri.to_s.split("/")[-1]
 user_page = mechanize.get("https://apply.likelion.org/apply/applicant/#{number}")
 user_info = user_page.search("#likelion_num  div:nth-child(2) > div.row.s_mt").text
-puts name[i] + ' ' + user_info.to_s.split(" ")[0] + ' ' + user_info.to_s.split(" ")[2]
+puts name[i] + ' ' + major[i] + ' ' + user_info.to_s.split(" ")[0] + ' ' + user_info.to_s.split(" ")[2]
 end
